e94519627e1178aca9e95f4c907fd09491a9c62b
Eduardo González <fonologico@gmail.com>
2016-10-30 00:29:21 +0200

    dynamic schema

--- tables/Posts.js
+++ tables/Posts.js
@@ -19,7 +19,7 @@ table.columns = {
     "userId": "string"
 };
 
-table.dynamicSchema = false;
+table.dynamicSchema = true;
 
 table.read.access = 'anonymous';
 table.update.access = 'authenticated';
